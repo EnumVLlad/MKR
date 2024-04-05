9cdcfdae78915caec3368cf07ded700e8fe74b84
Vlad Pylypenko <vladpilipenko640@gmail.com>
2024-04-05 13:53:38 +0300

    Count words and sentences

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def count_words_sentences(filename):
     # Використовуємо некаптурючу групу для розділових знаків речень
     sentence_pattern = r'[.!?]|(?:\.\.\.)'
     sentences = re.split(sentence_pattern, text)
-    sentences = [s for s in sentences if s and s.strip()]  # Фільтруємо None і пусті строки
+    sentences = [s for s in sentences if s and s.strip()]
 
     # Регулярний вираз для знаходження слів
     word_pattern = r'[,\s:;]+'
     words = re.split(word_pattern, text)
-    words = [w for w in words if w and w.strip()]  # Фільтруємо None і пусті строки
+    words = [w for w in words if w and w.strip()]
 
     return len(words), len(sentences)
 
