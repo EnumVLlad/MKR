2c8d59512d06394fb27b282460cb94212cf6ca81
Vlad Pylypenko <vladpilipenko640@gmail.com>
2024-04-05 14:42:37 +0300

    rebuilt main for saving results in txt format

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ def count_words_sentences(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         text = file.read()
 
-    # Використовуємо некаптурючу групу для розділових знаків речень
     sentence_pattern = r'[.!?]|(?:\.\.\.)'
     sentences = re.split(sentence_pattern, text)
     sentences = [s for s in sentences if s and s.strip()]  # Фільтруємо None і пусті строки
 
-    # Регулярний вираз для знаходження слів
     word_pattern = r'[,\s:;]+'
     words = re.split(word_pattern, text)
     words = [w for w in words if w and w.strip()]  # Фільтруємо None і пусті строки
@@ -19,5 +17,8 @@ def count_words_sentences(filename):
 # Приклад використання
 filename = 'text.txt'
 words_count, sentences_count = count_words_sentences(filename)
-print(f'Кількість слів: {words_count}')
-print(f'Кількість речень: {sentences_count}')
+
+# Записуємо результат у файл
+with open('readme.txt', 'w', encoding='utf-8') as output_file:
+    output_file.write(f'Кількість слів: {words_count}\n')
+    output_file.write(f'Кількість речень: {sentences_count}\n')
